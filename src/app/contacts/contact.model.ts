3330de336811cd3842c407070c13d2698cd4bd35
Danica <danica.juric@fsre.sum.ba>
2022-07-15 01:54:28 +0200

    created model for one contact

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,15 @@
+export class Contact{
+    public fname: string;
+    public lname: string;
+    public phonenr: string;
+    public caddress: string;
+    public imgpath: string;
+
+    constructor(fname: string, lname: string, phonenr: string, caddress: string, imgpath: string){ //is executed when we create a new instance of this class
+        this.fname=fname;
+        this.lname=lname;
+        this.phonenr=phonenr;
+        this.caddress=caddress;
+        this.imgpath=imgpath;
+    }
+}
