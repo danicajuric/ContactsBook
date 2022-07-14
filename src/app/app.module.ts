ba1dff78ea2b286c7245ae7b0644bc7bb4d35e9e
Danica <danica.juric@fsre.sum.ba>
2022-07-14 03:38:01 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactItemComponent } from './contacts/contact-list/contact-item/conta
     ContactsComponent,
     ContactListComponent,
     ContactDetailComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
