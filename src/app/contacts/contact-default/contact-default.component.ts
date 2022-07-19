c8f8bda02193d45a3ec8d39e05d6a105ab3a32cd
Danica <danica.juric@fsre.sum.ba>
2022-07-20 01:39:47 +0200

    default detail view component

---
+++ src/app/contacts/contact-default/contact-default.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-default',
+  templateUrl: './contact-default.component.html',
+  styleUrls: ['./contact-default.component.scss']
+})
+export class ContactDefaultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
