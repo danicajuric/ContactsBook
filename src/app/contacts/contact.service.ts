1c272d3126453289f85bd16ec071430a631a0aa4
Danica <danica.juric@fsre.sum.ba>
2022-07-21 11:11:05 +0200

    edited content to test pipe

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -4,8 +4,8 @@ export class ContactService {
     //contactSelected = new Subject<UserContact>(); dont need this
 
     private contacts: UserContact[] = [ //made private so it cant be directly addressed from the outside
-        new UserContact('Danicaaaaaaaaaaaaa', 'Juric', '+387000000', 'Some Address 88', "https://images.pexels.com/photos/12025241/pexels-photo-12025241.jpeg"),
-        new UserContact('Anđela', 'Juric', '+38ddddddddd7111111', 'Some Address 88', "https://images.pexels.com/photos/12498564/pexels-photo-12498564.jpeg")
+        new UserContact('danica', 'Juric', '+387000000', 'Some Address 88', "https://images.pexels.com/photos/12025241/pexels-photo-12025241.jpeg"),
+        new UserContact('Anđela', 'juric', '+38ddddddddd7111111', 'Some Address 88', "https://images.pexels.com/photos/12498564/pexels-photo-12498564.jpeg")
       ];
 
     getContacts(){ //get function which allows access from the outside
